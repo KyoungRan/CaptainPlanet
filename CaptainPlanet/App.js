9e23371cf23ba0143f523566891f3ec5864941a3
KyoungRan <kyoungrankang@gmail.com>
2017-09-20 19:37:07 +0900

    just saved

--- CaptainPlanet/App.js
+++ CaptainPlanet/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { ListView, Text, View } from 'react-native';
 
-<<<<<<< HEAD
 export default class App extends React.Component {
   render() {
     return (
@@ -20,24 +19,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 });
-=======
-class SelectTheme extends Component {
-	constructor() {
-		super();
-		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-		this.state = {
-			DataSource: ds.cloneWithRows(['row 1', 'row 2']),
-		}
-	}
-
-	render() {
-		return (
-			<View>
-				<Text>관심 있는 테마를 골라주세요.</Text>
-				<Text>나만의 서점을 추천해 드립니다.</Text>
-				<ListView dataSource={this.state.dataSource} renderRow={{rowData} => <Text>{rowData}</Text>} />
-			</View>
-		)
-	}
-}
->>>>>>> master
--- CaptainPlanet/screens/SelectTheme.js
+++ CaptainPlanet/screens/SelectTheme.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { ListView, Text, View } from 'react-native';
 
 class SelectTheme extends Component {
-	constructor() {
-		super();
-		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-		this.state = {
-			DataSource: ds.cloneWithRows(['row 1', 'row 2']),
-		}
-	}
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.state = {
+      DataSource: ds.cloneWithRows(['row 1', 'row 2'])
+    };
+  }
 
-	render() {
-		return (
-			<View>
-				<Text>관심 있는 테마를 골라주세요.</Text>
-				<Text>나만의 서점을 추천해 드립니다.</Text>
-				<ListView dataSource={this.state.dataSource} renderRow={{rowData} => <Text>{rowData}</Text>} />
-			</View>
-		)
-	}
+  render() {
+    return (
+      <View>
+        <Text>관심 있는 테마를 골라주세요.</Text>
+        <Text>나만의 서점을 추천해 드립니다.</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={rowData => <Text>{rowData}</Text>}
+        />
+      </View>
+    );
+  }
 }
